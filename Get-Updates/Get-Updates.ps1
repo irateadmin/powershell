0093bc59f84a9bd3800ba93319f87263fcf4d9f8
irateadmin <irateadmin@users.noreply.github.com>
2017-07-26 11:46:25 -0500

    Added user friendly messages

--- Get-Updates/Get-Updates.ps1
+++ Get-Updates/Get-Updates.ps1
@@ -16,11 +16,13 @@ $Output = ForEach ($server in $user_input_computer) {
     catch   
   
       {  
-    Add-content $server -path "$env:USERPROFILE\Desktop\Unreachable_Servers.txt" 
+    Add-content $server -path "$env:USERPROFILE\Desktop\Unreachable_Machines.txt"
+    Write-Host "Some machines were unreachable. The list is located here: '$env:USERPROFILE\Desktop\Unreachable_Machines.txt'" -ForegroundColor Red -BackgroundColor Black
       }   
     } 
     #Write $Output to .csv
-    $Output | Export-CSV $env:USERPROFILE\Desktop\Installed_Updates_Last_"$days"_Days.csv 
+    $Output | Export-CSV $env:USERPROFILE\Desktop\Installed_Updates_Last_"$days"_Days.csv
+    Write-Host "Your scan is complete. The list is located here: '$env:USERPROFILE\Desktop\Installed_Updates_Last_"$days"_Days.csv'" -ForegroundColor Green 
     } 
 
     function nouserinput {
@@ -35,20 +37,33 @@ $Output = ForEach ($server in $user_input_computer) {
     catch   
   
       {  
-    Add-content $server -path "$env:USERPROFILE\Desktop\Unreachable_Servers.txt" 
+    Add-content $server -path "$env:USERPROFILE\Desktop\Unreachable_Machines.txt" 
+    Write-Host "Some machines were unreachable. The list is located here: '$env:USERPROFILE\Desktop\Unreachable_Machines.txt'" -ForegroundColor Red -BackgroundColor Black
       }   
     }
     #Write $Output to .csv
-    $Output | Export-CSV $env:USERPROFILE\Desktop\Installed_Updates_Last_"$days"_Days.csv 
+    $Output | Export-CSV $env:USERPROFILE\Desktop\Installed_Updates_Last_"$days"_Days.csv
+    Write-Host "Your scan is complete. The list is located here: '$env:USERPROFILE\Desktop\Installed_Updates_Last_"$days"_Days.csv'" -ForegroundColor Green 
     } 
-
+# Check if servers.txt exists and save result
 $testpathservers = Test-Path $env:USERPROFILE\Desktop\servers.txt
 
+# Check if Unreachable_Machines.txt already exists and remove it
+$testpathunreachable_machines = Test-Path $env:USERPROFILE\Desktop\Unreachable_Machines.txt
+If ($testpathunreachable_machines -eq $true) {
+    Remove-Item $env:USERPROFILE\Desktop\Unreachable_Machines.txt
+    }
+
 If (-not $testpathservers) {
+    Write-Host "A servers.txt file was not found here: '$env:USERPROFILE\Desktop\servers.txt'" -ForegroundColor Red -BackgroundColor Black
+    Write-Host "You may enter a single computer name to scan." -ForegroundColor Red -BackgroundColor Black
     $user_input_computer = Read-Host "What computer would you like to scan?"
-    userinput    
+    Write-Host "This may take several minutes to complete your scan. Please be patient." -ForegroundColor Green
+    userinput  
     }
     else {
     $servers = Get-Content $env:USERPROFILE\Desktop\servers.txt
     nouserinput
     }
+    Write-Host " "
+    Read-Host "Press Enter to exit..."
